a33e8f27f470f2c7c6d151a98ddcdb6ec0887373
HildoCostaa <hildohenrik2004@gmail.com>
2026-07-05 15:42:01 -0300

    estrutura base front-end

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+==========================================
+
+DSJ CONSTRUTORA
+
+Arquivo principal
+
+==========================================
+*/
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    console.log("DSJ Construtora inicializada.");
+
+});
